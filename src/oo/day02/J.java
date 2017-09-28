ac418117bb01c529562f2d176cc5ae448949bf3f
bret001 <903692290@qq.com>
2017-09-29 00:12:32 +0800

    update0928-home_pc

--- src/oo/day02/J.java
+++ src/oo/day02/J.java
@@ -1,33 +1,11 @@
 package oo.day02;
 
-public class J {
-	Cell[] cell;
-	J(int row,int col) {
-		cell = new Cell[4];
-		cell[0]= new Cell(row,col);
-		cell[1]= new Cell(row+1,col);
-		cell[2]= new Cell(row+2,col);
-		cell[3]= new Cell(row+2,col-1);
-	}
-	void drop(){
-		for (int i=0;i<cell.length;i++) {
-			cell[i].row++;
-		}
-	}
-	void moveLeft(){
-		for(int i=0;i<cell.length;i++) {
-			cell[i].col--;
-		}
-	}
-	void moveRight(){
-		for (int i=0;i<cell.length;i++){
-			cell[i].col++;
-		}
-	}
-	void print(){
-		for (int i=0;i<cell.length;i++){
-			System.out.println(cell[i].getCellInfo());
-		}
-	}
+public class J extends Tetromino{
 
+	J(int row, int col) {
+		cell[0] = new Cell(row, col);
+		cell[1] = new Cell(row + 1, col);
+		cell[2] = new Cell(row + 2, col);
+		cell[3] = new Cell(row + 2, col - 1);
+	}
 }
---
+++ src/oo/day02/Tetromino.java
@@ -0,0 +1,33 @@
+package oo.day02;
+
+public class Tetromino {
+	Cell[] cell;
+
+	Tetromino() {
+		cell = new Cell[4];
+	}
+
+	void drop() {
+		for (int i = 0; i < cell.length; i++) {
+			cell[i].row++;
+		}
+	}
+ 
+	void moveLeft() {
+		for (int i = 0; i < cell.length; i++) {
+			cell[i].col--;
+		}
+	}
+
+	void moveRight() {
+		for (int i = 0; i < cell.length; i++) {
+			cell[i].col++;
+		}
+	}
+
+	void print() {
+		for (int i = 0; i < cell.length; i++) {
+			System.out.println(cell[i].getCellInfo());
+		}
+	}
+}
